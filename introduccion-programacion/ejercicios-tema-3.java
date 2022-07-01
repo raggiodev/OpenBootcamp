ddd2f18b3583abdbd65c9df776492925729ffbba
Fernando Andrés Raggio <fernandoandresraggio@gmail.com>
2022-06-30 21:56:46 -0300

    Create ejercicios-tema-3.java

---
+++ introduccion-programacion/ejercicios-tema-3.java
@@ -0,0 +1,27 @@
+// Ejercicios Tema 3 - Curso de Introduccion a la programacion - OpenBootcamp | @JoeTheorium - Raggio Fernando Andres
+public class Main {
+    
+    public static void main(String[] args) {
+      suma(10,-20,30);
+      
+      Coche coche = new Coche();
+      
+      coche.agregarPuerta();
+      System.out.println("El numero de puertas que posee el coche ahora es: " + coche.puertas + ".");
+    
+    }
+    // Primera parte:
+    public static void suma(int num1, int num2, int num3) {
+        int resultado;
+        resultado = num1 + num2 + num3;
+        System.out.println("El resultado de la suma entre estos tres numeros es: " + resultado + ".");
+    }
+}
+//Segunda parte:
+class Coche {
+    public int puertas = 4;
+    
+    public void agregarPuerta() {
+        this.puertas++;
+    }
+}
