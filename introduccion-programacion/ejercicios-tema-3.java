3780670b405cd13357019f92161f6f436ac4f1b0
Fernando Andrés Raggio <fernandoandresraggio@gmail.com>
2022-07-04 21:00:57 -0300

    -2 spaces per line

--- introduccion-programacion/ejercicios-tema-3.java
+++ introduccion-programacion/ejercicios-tema-3.java
@@ -1,27 +1,27 @@
-// Ejercicios Tema 3 - Curso de Introduccion a la programacion - OpenBootcamp | @JoeTheorium - Raggio Fernando Andres
+// Ejercicios Tema 3 - Curso de Introducción a la programación - OpenBootcamp | @JoeTheorium - Raggio Fernando Andres
 public class Main {
-    
-    public static void main(String[] args) {
-      suma(10,-20,30);
+  
+  public static void main(String[] args) {
+    suma(10,-20,30);
       
-      Coche coche = new Coche();
+    Coche coche = new Coche();
       
-      coche.agregarPuerta();
-      System.out.println("El numero de puertas que posee el coche ahora es: " + coche.puertas + ".");
+    coche.agregarPuerta();
+    System.out.println("El número de puertas que posee el coche ahora es: " + coche.puertas + ".");
+  }
+  // Primera parte:
+  public static void suma(int num1, int num2, int num3) {
+    int resultado;
     
-    }
-    // Primera parte:
-    public static void suma(int num1, int num2, int num3) {
-        int resultado;
-        resultado = num1 + num2 + num3;
-        System.out.println("El resultado de la suma entre estos tres numeros es: " + resultado + ".");
-    }
+    resultado = num1 + num2 + num3;
+    System.out.println("El resultado de la suma entre estos tres números es: " + resultado + ".");
+  }
 }
 //Segunda parte:
 class Coche {
-    public int puertas = 4;
+  public int puertas = 4;
     
-    public void agregarPuerta() {
-        this.puertas++;
-    }
+  public void agregarPuerta() {
+    this.puertas++;
+  }
 }
