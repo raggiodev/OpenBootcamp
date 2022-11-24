5da8bd339083d4622b7c0a2ca16cb40af94c5140
Fernando Andrés Raggio <fernandoandresraggio@gmail.com>
2022-11-23 22:13:11 -0300

    Create index.js

---
+++ JavaScript-Basico/01-Introduction/index.js
@@ -0,0 +1,4 @@
+// Ejercicio 01 - Curso de JavaScript Básico - OpenBootcamp | @JoeTheorium - Raggio Fernando Andrés
+
+// Mensaje solicitado por el enunciado
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo");
