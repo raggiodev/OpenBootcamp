de51a3afabfec7e38412e6d42298a49232799e88
Fernando Andrés Raggio <fernandoandresraggio@gmail.com>
2022-11-24 22:41:30 -0300

    Create syntax.js

---
+++ JavaScript-Basico/02-Syntax/syntax.js
@@ -0,0 +1,13 @@
+// Ejercicio 02 - Curso de JavaScript Básico - OpenBootcamp | @JoeTheorium - Raggio Fernando Andrés
+const list = [
+    "Joe Fernando Andrés Raggio",
+    23,
+    true,
+    new Date(1999, 1, 9),
+    favorite_book = {
+        title: "The Adventures of Sherlock Holmes",
+        author: "Arthur Conan Doyle",
+        date: new Date(1982, 10, 14),
+        url: "https://en.wikisource.org/wiki/The_Adventures_of_Sherlock_Holmes_(1892,_US)"
+    },
+];
