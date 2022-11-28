dcffae0eb70c669d4c67751cd5e6b25822ba3685
Fernando Andrés Raggio <fernandoandresraggio@gmail.com>
2022-11-28 18:23:27 -0300

    Create factorial-while.js

---
+++ JavaScript-Basico/03-Control_Structures/factorial-while.js
@@ -0,0 +1,10 @@
+// Ejercicio 03-2 - Curso de JavaScript Básico - OpenBootcamp | @JoeTheorium - Raggio Fernando Andrés
+let factorial = 1;
+let i = 10;
+
+while (i > 1) {
+    factorial *= i;
+    i--;
+}
+
+console.log(factorial); // Factorial de 10 = 3628800
