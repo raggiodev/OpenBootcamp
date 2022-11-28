77670266b4b5effd3d0748e58083599b9ad11020
Fernando Andrés Raggio <fernandoandresraggio@gmail.com>
2022-11-28 18:21:11 -0300

    Create factorial-for.js

---
+++ JavaScript-Basico/03-Control_Structures/factorial-for.js
@@ -0,0 +1,8 @@
+// Ejercicio 03 - Curso de JavaScript Básico - OpenBootcamp | @JoeTheorium - Raggio Fernando Andrés
+let factorial = 1;
+
+for (let i = 10; i > 0; i--) {
+    factorial *= i;
+}
+
+console.log(factorial); // Factorial de 10 = 3628800
