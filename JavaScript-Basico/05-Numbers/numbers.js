3a559b76b33b860f323faa7b3db7e1ee4201272f
Fernando Andrés Raggio <fernandoandresraggio@gmail.com>
2022-11-30 20:34:22 -0300

    Create numbers.js

---
+++ JavaScript-Basico/05-Numbers/numbers.js
@@ -0,0 +1,10 @@
+// Ejercicio 05 - Curso de JavaScript Básico - OpenBootcamp | @JoeTheorium - Raggio Fernando Andrés
+let altura_cm = 181;
+let altura_m = 1.81;
+let peso_kg = 77.0;
+
+let altura_red_arriba = Math.ceil(altura_m); // 2m.
+let peso_red_abajo = Math.floor(peso_kg); // 77kg.
+
+let igualdad = (Number.MAX_VALUE + 1) === Number.MAX_VALUE; // true
+console.log(peso_red_abajo);
