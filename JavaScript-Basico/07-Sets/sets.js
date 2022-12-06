b735da40fc662d656917d7f3d5817a10ca96c8a9
Fernando Andrés Raggio <fernandoandresraggio@gmail.com>
2022-12-06 20:01:02 -0300

    Delete JavaScript-Basico/07-Sets directory

--- JavaScript-Basico/07-Sets/sets.js
+++
@@ -1,7 +0,0 @@
-// Ejercicio 07-1 - Curso de JavaScript Básico - OpenBootcamp | @JoeTheorium - Raggio Fernando Andrés
-const familia = new Set(["Teresa", "Gabriela", "Alfonso", "Fernando", "Guadalupe"]);
-
-familia.add("Fernando");
-familia.add("JavaScript");
-
-console.log(familia); // Set { 'Teresa', 'Gabriela', 'Alfonso', 'Fernando', 'Guadalupe', JavaScript' }
