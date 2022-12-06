3e5a561c05e709acbe16d2286fdf38fea0d105d6
Fernando Andrés Raggio <fernandoandresraggio@gmail.com>
2022-12-06 20:27:55 -0300

    Create chrome_console.js

---
+++ JavaScript-Basico/07-Sets_Objects_Dates/chrome_console.js
@@ -0,0 +1,4 @@
+// Ejercicio 07-4 - Curso de JavaScript Básico - OpenBootcamp | @JoeTheorium - Raggio Fernando Andrés
+const edad = prompt("Cuál es tu edad?");
+// Después de la respuesta del usuario, en la consola:
+console.log(edad); // edad_ingresada
