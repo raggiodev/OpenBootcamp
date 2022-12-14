6c4d9e50a327839180168206780fbe90fdf49109
Fernando Andrés Raggio <fernandoandresraggio@gmail.com>
2022-12-14 20:31:07 -0300

    Create controller.js

---
+++ JavaScript-Basico/10-Node_Modules/controller.js
@@ -0,0 +1,8 @@
+// Ejercicio 10 - Curso de JavaScript Básico - OpenBootcamp | @JoeTheorium - Raggio Fernando Andrés
+export function suma(a, b) {
+    return a + b;
+}
+
+export function multiplica(a, b) {
+    return a * b;
+}
