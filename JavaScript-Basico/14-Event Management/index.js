d2044440e96db37a0cd03068235bd0692707a84b
Fernando Andrés Raggio <fernandoandresraggio@gmail.com>
2023-01-10 19:13:57 -0300

    Create index.js

---
+++ JavaScript-Basico/14-Event Management/index.js
@@ -0,0 +1,18 @@
+<!-- Ejercicio 14 - Curso de JavaScript Básico - OpenBootcamp | @JoeTheorium - Raggio Fernando Andrés -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio: Gestión de eventos</title>
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
+</head>
+
+<body>
+    <button>Botón</button>
+</body>
+
+<script src="index.js"></script>
+</html>
