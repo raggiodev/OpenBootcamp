7d80b69ae0a18c0d561f500ce876f5c948935863
Fernando Andrés Raggio <fernandoandresraggio@gmail.com>
2023-01-11 21:53:11 -0300

    Delete JavaScript-Basico/14-Event Management directory

--- JavaScript-Basico/14-Event Management/index.js
+++
@@ -1,8 +0,0 @@
-// Ejercicio 14 - Curso de JavaScript Básico - OpenBootcamp | @JoeTheorium - Raggio Fernando Andrés
-const boton = document.querySelector("button")
-
-boton.addEventListener("click", () => alert("Se dió CLICK en el botón!"))
-
-$("button").click(function() {
-    console.log("Hola, estoy utilizando jQuery!")
-})
